21c3acc25887becb3913225ae51c5f162ed07c35
Simon Heath <simon.heath@gmail.com>
2018-05-19 15:16:56 +0200

    Fix compilation errors

--- tools/gemBS_plugins/snpxtr.mk
+++ tools/gemBS_plugins/snpxtr.mk
@@ -1,2 +1,2 @@
 plugins/snpxtr.so: plugins/snpxtr.c version.h version.c ../gemBS_plugins/utils.c ../gemBS_plugins/compress.c ../gemBS_plugins/compress.h ../gemBS_plugins/utils.h ../gemBS_plugins/uthash.h ../gemBS_plugins/snpxtr.h
-	$(CC) $(PLUGIN_FLAGS) $(CFLAGS) $(ALL_CPPFLAGS) -I ../gemBS_plugins $(EXTRA_CPPFLAGS) $(LDFLAGS) -o $@ ../gemBS_plugins/utils.c ../gemBS_plugins/compress.c version.c $< $(LIBS)
+	$(CC) --std=gnu11 $(PLUGIN_FLAGS) $(CFLAGS) $(ALL_CPPFLAGS) -I ../gemBS_plugins $(EXTRA_CPPFLAGS) $(LDFLAGS) -o $@ ../gemBS_plugins/utils.c ../gemBS_plugins/compress.c version.c $< $(LIBS)
