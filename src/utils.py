2e04a309e400a4e36b3193f70543476580356e83
Simon Heath <simon.heath@gmail.com>
2018-06-08 12:26:01 +0200

    Reverse order of checking for process end in pipelines to allow detection of when a child dies"

--- src/utils.py
+++ src/utils.py
@@ -356,7 +356,7 @@ class ProcessWrapper(object):
                     if r is not None:
                         r.wait()
             exit_value = 0
-            for i, process in enumerate(self.processes):
+            for process in reverse(self.processes):
                 ev = process.wait()
                 if exit_value is not 0:
                     exit_value = ev
